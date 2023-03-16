dccaac4b153acaaf7179e618bf586578573ef5b2
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-03-16 09:32:02 +0000

    build: remove tailwind plugin in production

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
+const IS_DEVELOPMENT = process.env.NODE_ENV !== 'production';
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    IS_DEVELOPMENT && require('tailwindcss-debug-screens'),
+  ].filter(Boolean),
 }
